b8ee803d92dc0746f3afc6b5f61281c64caf6880
aatmdelissen <2767694+aatmdelissen@users.noreply.github.com>
2020-07-13 13:52:36 +0200

    EinSum: Exception for trace, removed possible errors with ->

--- pyModular/block_library.py
+++ pyModular/block_library.py
@@ -74,41 +74,65 @@ class EinSum(Module):
     """ General linear algebra module which uses the numpy function einsum
 
     Many linear algebra multiplications can be implemented using this module:
+    Vector sum             "i->"          y = sum(u)
     Elementwise multiply   "i,i->i"       w = u * v
     Dot product            "i,i->"        y = np.dot(u,v)
     Outer product          "i,j->ij"      A = np.outer(u,v)
     Matrix-vector product  "ij,j->i"      x = A.dot(b)
-    Weighted dot-product   "i,ij,j->"     y = b.dot(A.dot(b))
+    Quadratic form         "i,ij,j->"     y = b.dot(A.dot(b))
     Matrix-matrix product  "ij,ij->ij"    C = A * B
     Transpose matrix prod. "ji,ij->ij"    C = A.T * B
     Matrix projection      "ji,jk,kl->il" B = V.T.dot(A.dot(V))
+
+    Many more advanced operations are also supported, with exception of expressions with repeated indices (e.g. iij->ij)
+
+    More info:
+    https://stackoverflow.com/questions/26089893/understanding-numpys-einsum
+    https://obilaniu6266h16.wordpress.com/2016/02/04/einstein-summation-in-numpy/
     """
     def _prepare(self, expression):
         self.expr = expression
-        cmd = self.expr.split("->")  # TODO if no -> is given
-        self.indices_in = cmd[0].split(",")
-        self.indices_out = cmd[1]
+        cmd = self.expr.split("->")
+        self.indices_in = [s.strip() for s in cmd[0].split(",")]
+        self.indices_out = cmd[1] if "->" in self.expr else ''
 
     def _response(self, *args):
         return [np.einsum(self.expr, *args)]
 
     def _sensitivity(self, df_in):
+        n_in = len(self.sig_in)
+
+        if (self.indices_out == '') and n_in == 1:
+            # In case expression has only one input and scalar output, e.g. "i->", "ij->", the output size cannot
+            # be deducted. Therefore we add these exceptions
+            if len(set(self.indices_in[0])) < len(self.indices_in[0]):
+                # exception for repeated indices (e.g. trace, diagonal summing)
+                if self.sig_in[0].get_state().ndim > 2:
+                    raise TypeError(
+                        "Sensitivities for repeated incides '{}' not supported for any other than trace 'ii->'."
+                        .format(self.expr))
+                mat = np.zeros_like(self.sig_in[0].get_state())
+                np.fill_diagonal(mat, 1.0)
+            else:
+                mat = np.ones_like(self.sig_in[0].get_state())
+            return np.conj(np.conj(df_in) * mat)
+
+        for ind_in in self.indices_in:
+            if len(set(ind_in)) < len(ind_in):
+                raise TypeError("Sensitivities for repeated incides '{}' not supported for any other than trace 'ii->'."
+                                .format(self.expr))
+
         df_out = []
-        for ar in range(len(self.sig_in)):
+        for ar in range(n_in):
             ind_in = [self.indices_out]
             ind_in += [elem for i, elem in enumerate(self.indices_in) if i != ar]
             arg_in = [s.get_state() for i, s in enumerate(self.sig_in) if i != ar]
             ind_out = self.indices_in[ar]
-            if (self.indices_out == '') and len(self.sig_in) == 1:
-                mat = np.ones_like(self.sig_in[ar].get_state())
-                df_in_sep = df_in * mat
-                ind_in = [self.indices_in[ar]]
-                op = ",".join(ind_in)+"->"+ind_out
-                da_i = np.einsum(op, df_in_sep, *arg_in)
-            else:
-                op = ",".join(ind_in)+"->"+ind_out
-                da_i = np.einsum(op, df_in, *arg_in)
-            df_out.append(da_i)
+
+            da_i = np.zeros_like(self.sig_in[ar].get_state())
+            op = ",".join(ind_in)+"->"+ind_out
+            np.einsum(op, np.conj(df_in), *arg_in, out=da_i)
+            df_out.append(np.conj(da_i))
         return df_out
 
 
