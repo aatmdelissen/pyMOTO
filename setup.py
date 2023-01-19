b9971d711a2dfe4f5c71a555a0f762eb84eb1031
aatmdelissen <2767694+aatmdelissen@users.noreply.github.com>
2023-01-19 14:54:06 +0100

    Update setup.py for PyPI repository packaging

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup
 
-setup(name='pymoto',
-      version='1.0',
-      description='A modular framework to perform Topology Optimization in a flexible manner',
-      long_description='Semi-automatic differentiation',
-      keywords='topology optimization framework modular blocks',
-      url='https://github.com/aatmdelissen/pyModular',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+      name='pymoto',
+      version='1.0.0',
       author='Arnoud Delissen',
-      author_email='arnouddelissen@gmail.com',
-      license='MIT',
+      author_email='arnouddelissen+pymoto@gmail.com',
+      description='A modular framework to perform Topology Optimization in a flexible manner',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      keywords='Topology Optimization Framework Modular Blocks Pipeline Structural Generative Design',
+      url='https://github.com/aatmdelissen/pyMOTO',
       packages=['pymoto'],
+      package_data={'pymoto': ['*', 'common/*', 'modules/*']},
       install_requires=['numpy', 'sympy', 'scipy', 'matplotlib'],
-      zip_safe=False,
-      )
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+            "License :: OSI Approved :: MIT License",
+            "Topic :: Scientific/Engineering"
+      ],
+)
