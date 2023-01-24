be493d3280e080b5232609c3bed54917d825290e
aatmdelissen <2767694+aatmdelissen@users.noreply.github.com>
2023-01-24 17:14:30 +0100

    Update complex documentation

--- pymoto/modules/complex.py
+++ pymoto/modules/complex.py
@@ -1,26 +1,39 @@
 """
-Definition of adjoint variable for z = x+iy :
-∂/∂z  = 1/2(∂/∂x - i ⋅ ∂/∂y) (<-- this is stored in the adjoint variable to keep the the rule adj_z := ∂f/∂z )
-∂/∂z^* = 1/2(∂/∂x + i ⋅ ∂/∂y) 
+Definition of adjoint variable for :math:`z = x+iy` :
 
-Useful identities (z∈C and s∈C)
-∂s^* / ∂z^* = (∂s/∂z)^*
-∂s^* / ∂z   = (∂s/∂z^*)^*
-In case f∈R
-∂f/∂z^* = (∂f/∂z)^*
+  - :math:`\\frac{\partial}{\partial z} = \\frac{1}{2}\left( \\frac{\partial}{\partial x} - i \\frac{\partial}{\partial y}\\right)` (<-- this is stored in the adjoint variable (:attr:`Signal.sensitivity`) to keep the the rule ``adj_z`` := ∂f/∂z )
+  - :math:`\\frac{\partial}{\partial z^*} = \\frac{1}{2}\left( \\frac{\partial}{\partial x} + i \\frac{\partial}{\partial y}\\right)`
 
-Chain rule: z∈C -> s∈C: ∂f/∂z = ∂f/∂s ⋅ ∂s/∂z + ∂f/∂s^*     ⋅ ∂s^*/∂z 
-                        z_adj = s_adj ⋅ ∂s/∂z + conj(s_adj) ⋅ d∂^*/∂z
-                        
-Mapping for z∈C -> r∈R: ∂f/∂z = 2 ⋅ ∂f/dr ⋅ ∂r/∂z
-                        z_adj = 2 ⋅ s_adj ⋅ ∂s/∂z
-                        
-Mapping for r∈R -> z∈C: ∂f/∂r = 2 ⋅ Re( ∂f/∂s ⋅ ∂s/∂r )
-                        z_adj = 2 ⋅ Re( s_adj ⋅ ∂s/∂r )
+Useful identities (:math:`z\in\mathbb{C}` and :math:`s\in\mathbb{C}`)
 
-References: 
-https://pytorch.org/docs/stable/notes/autograd.html
-https://arxiv.org/pdf/0906.4835.pdf
+  - :math:`\\frac{\partial s^*}{\partial z^*} = \left( \\frac{\partial s}{\partial z} \\right)^*`
+  - :math:`\\frac{\partial s^*}{\partial z} = \left( \\frac{\partial s}{\partial z^*} \\right)^*`
+
+In case :math:`f\in\mathbb{R}`
+
+  - :math:`\\frac{\partial f}{\partial z^*} = \left( \\frac{\partial f}{\partial z} \\right)^*`
+
+The chain rule for a mapping from :math:`z\in\mathbb{C}\\rightarrow s\in\mathbb{C}\\rightarrow f` can be seen as the
+contributions of two independent variables, e.g. :math:`s` and :math:`s^*`:
+
+:math:`\\frac{\partial f}{\partial z} = \\frac{\partial f}{\partial s}\\frac{\partial s}{\partial z} + \\frac{\partial f}{\partial s^*}\\frac{\partial s^*}{\partial z}`
+
+In case the intermediate variable is real, thus :math:`z\in\mathbb{C}\\rightarrow r\in\mathbb{R}\\rightarrow f`, the
+chain rule reduces to
+
+:math:`\\frac{\partial f}{\partial z} = 2 \\frac{\partial f}{\partial r}\\frac{\partial r}{\partial z}`,
+
+which may seem counter-intuitive, but compensates for the initial factor of 1/2.
+
+For a mapping from real to complex, thus :math:`r\in\mathbb{R}\\rightarrow z\in\mathbb{C}\\rightarrow f`, the chain rule
+becomes
+
+:math:`\\frac{\partial f}{\partial z} = 2 \\text{Re}\left( \\frac{\partial f}{\partial s}\\frac{\partial s}{\partial r} \\right)`.
+
+References:
+  - Sarason (2007). Complex function theory. American Mathematical Society.
+  - `Delgado (2009). The complex gradient operator and the CR-calculus <https://arxiv.org/pdf/0906.4835.pdf>`_
+  - `Pytorch AutoGrad <https://pytorch.org/docs/stable/notes/autograd.html>`_
 """
 
 import numpy as np
@@ -28,7 +41,7 @@ from pymoto import Module
 
 
 class MakeComplex(Module):
-    """ Makes a complex variable from two real inputs z = x + jy """
+    """ Makes a complex variable from two real inputs :math:`z(x,y) = x + iy` """
     def _response(self, x, y):
         return x + 1j*y
 
@@ -37,7 +50,7 @@ class MakeComplex(Module):
 
 
 class RealPart(Module):
-    """ Takes the real part of a complex value x = Re(z) """
+    """ Takes the real part of a complex value :math:`x = \\text{Re}(z)` """
     def _response(self, z):
         return np.real(z)
 
@@ -46,7 +59,7 @@ class RealPart(Module):
 
 
 class ImagPart(Module):
-    """ Takes the imaginary part of a complex value y = Im(z) """
+    """ Takes the imaginary part of a complex value :math:`y = \\text{Im}(z)` """
     def _response(self, z):
         return np.imag(z)
 
@@ -55,7 +68,7 @@ class ImagPart(Module):
 
 
 class ComplexNorm(Module):
-    """ Takes the complex norm """
+    """ Takes the complex norm :math:`n = z z^*` """
     def _response(self, z):
         return np.absolute(z)
 
