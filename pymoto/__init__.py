ca788232a5571f4ec8dad6efaa902990e0b2d552
Arnoud <2767694+aatmdelissen@users.noreply.github.com>
2023-03-08 15:12:03 +0100

    Update __init__.py
    Test version

--- pymoto/__init__.py
+++ pymoto/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0'
+__version__ = '1.0.1_test_a'
 
 from .core_objects import Signal, Module, Network, make_signals
 from .routines import finite_difference, minimize_oc, minimize_mma
