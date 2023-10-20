8a68ac9e67b8a661692ac14eb74e486426804094
Stijn Koppen <s.koppen@tudelft.nl>
2023-10-20 22:40:41 +0200

    separation of stress calculation, von mises and constraint aggregation

--- examples/topology_optimization/ex_volume_stress.py
+++ examples/topology_optimization/ex_volume_stress.py
@@ -1,8 +1,14 @@
 """
-Example of the design of cantilever for minimum volume subjected to displacement constraint.
+Example of the design of cantilever for minimum volume subjected to stress constraints.
 
 References:
-    None? s.koppen@tudelft.nl
+
+Implemented by @artofscience (s.koppen@tudelft.nl), based on:
+
+Verbart, A., Langelaar, M., & Keulen, F. V. (2017).
+A unified aggregation and relaxation approach for stress-constrained topology optimization.
+Structural and Multidisciplinary Optimization, 55, 663-679.
+DOI: https://doi.org/10.1007/s00158-016-1524-0
 """
 
 import numpy as np
@@ -11,38 +17,52 @@ import pymoto as pym
 from pymoto.modules.assembly import get_B, get_D
 
 # Problem settings
-nx, ny = 100, 60  # Domain size
-xmin, filter_radius, volfrac = 1e-6, 1.5, 1.0
+nx, ny = 100, 50  # Domain size
+xmin, filter_radius, volfrac = 1e-6, 2, 1.0
 
 scaling_objective = 10.0
-maximum_vm_stress = 0.4
-
-
-class StressConstraints(pym.Module):
-    def _prepare(self, max_stress):
-        self.max_stress = max_stress
-
-    def _response(self, stress_vm):
-        return (stress_vm / self.max_stress) - 1
-
-    def _sensitivity(self, dfdv):
-        return dfdv / self.max_stress
+maximum_vm_stress = 0.5
 
 
 class ConstraintAggregation(pym.Module):
+    """
+    Unified aggregation and relaxation.
+
+    Implemented by @artofscience (s.koppen@tudelft.nl), based on:
+
+    Verbart, A., Langelaar, M., & Keulen, F. V. (2017).
+    A unified aggregation and relaxation approach for stress-constrained topology optimization.
+    Structural and Multidisciplinary Optimization, 55, 663-679.
+    DOI: https://doi.org/10.1007/s00158-016-1524-0
+    """
 
     def _prepare(self, P=10):
         self.P = P
 
-    def _response(self, scaled_stress_constraints):
-        self.g = scaled_stress_constraints + 1
-        self.f = self.g ** self.P
-        self.e = (1 / len(self.f)) * np.sum(self.f)
-        d = self.e ** (1 / self.P)
-        return d - 1
+    def _response(self, x):
+        self.n = len(x)
+        self.x = x
+        self.y = self.x + 1
+        self.z = self.y ** self.P
+        z = ((1 / len(self.x)) * np.sum(self.z)) ** (1 / self.P)  # P-mean aggregation function
+        return z - 1
 
     def _sensitivity(self, dfdc):
-        return (dfdc / len(self.f)) * self.e ** (1 / self.P - 1) * self.g ** (self.P - 1)
+        return (dfdc / self.n) * (np.sum(self.z) / self.n) ** (1 / self.P - 1) * self.y ** (self.P - 1)
+
+
+class VonMises(pym.Module):
+    def _prepare(self, *args, **kwargs):
+        # Vandermonde matrix
+        self.V = np.array([[1, -0.5, 0], [-0.5, 1, 0], [0, 0, 3]])
+
+    def _response(self, x):
+        self.x = x
+        self.y = (x.dot(self.V) * x).sum(1)
+        return np.sqrt(self.y)
+
+    def _sensitivity(self, dfdv):
+        return dfdv[:, np.newaxis] * (self.y ** (-0.5))[:, np.newaxis] * self.x.dot(self.V)
 
 
 class Stress(pym.Module):
@@ -52,9 +72,6 @@ class Stress(pym.Module):
 
         # Constitutive model
         self.D = siz[2] * get_D(E, nu, plane.lower())
-
-        # Vandermonde matrix
-        self.V = np.array([[1, -0.5, 0], [-0.5, 1, 0], [0, 0, 3]])
 
         # Numerical integration
         self.B = np.zeros((3, 8), dtype=float)
@@ -69,15 +86,13 @@ class Stress(pym.Module):
     def _response(self, u):
         self.elemental_strain = self.B.dot(u[self.dofconn].transpose())
         self.elemental_strain[2, :] *= 2  # voigt notation
-        self.elemental_stress = self.D.dot(self.elemental_strain).transpose()
-        self.stress_vm0 = (self.elemental_stress.dot(self.V) * self.elemental_stress).sum(1)
-        return np.sqrt(self.stress_vm0)
+        return self.D.dot(self.elemental_strain).transpose()
 
     def _sensitivity(self, dfdv):
-        sens = dfdv[:, np.newaxis] * (self.stress_vm0 ** (-0.5))[:, np.newaxis] * self.elemental_stress.dot(self.V)
-        dgdsstrainmat = np.einsum('jk,kl->jl', sens, self.D)
+        dgdsstrainmat = np.einsum('jk,kl->jl', dfdv, self.D)
         dgdsstrainmat[:, 2] *= 2
         dgdue = np.einsum('ij,jl->il', dgdsstrainmat, self.B)
+
         y = np.zeros(self.domain.nnodes * 2)
         for i in range(0, self.domain.nel):
             y[self.dofconn[i, :]] += dgdue[i, :]
@@ -116,9 +131,11 @@ if __name__ == "__main__":
     s_displacement = fn.append(pym.LinSolve([s_K, s_force]))
 
     # Calculate stress
-    s_stress_vm = fn.append(Stress([s_displacement], domain=domain))
+    s_stress = fn.append(Stress([s_displacement], domain=domain))
+    s_stress_vm = fn.append(VonMises([s_stress]))
+    s_stress_constraints = fn.append(pym.Scaling([s_stress_vm], maxval=maximum_vm_stress, scaling=1.0))
 
-    s_stress_constraints = fn.append(StressConstraints([s_stress_vm], max_stress=maximum_vm_stress))
+    # s_stress_constraints = fn.append(StressConstraints([s_stress_vm], max_stress=maximum_vm_stress))
     s_stress_constraints_scaled = fn.append(
         pym.EinSum([s_filtered_variables, s_stress_constraints], expression='i,i->i'))
     s_stress_constraint = fn.append(ConstraintAggregation([s_stress_constraints_scaled], P=10))
