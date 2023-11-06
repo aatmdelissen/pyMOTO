b5ce760792ba89af50b0d175e8fd8672897c2b7b
aatmdelissen <2767694+aatmdelissen@users.noreply.github.com>
2023-11-06 19:48:35 +0100

    Cleanup init file
    Signed-off-by: aatmdelissen <2767694+aatmdelissen@users.noreply.github.com>

--- pymoto/__init__.py
+++ pymoto/__init__.py
@@ -1,24 +1,28 @@
 __version__ = '1.1.0'
 
 from .common.domain import DomainDefinition
+
 # Imports from common
 from .common.dyadcarrier import DyadCarrier
 from .common.mma import MMA
-from .common.solvers import matrix_is_complex, matrix_is_diagonal, matrix_is_symmetric, matrix_is_hermitian, \
-    LinearSolver, LDAWrapper
+from .common.solvers import matrix_is_complex, matrix_is_diagonal, matrix_is_symmetric, matrix_is_hermitian, LinearSolver, LDAWrapper
 from .common.solvers_dense import SolverDiagonal, SolverDenseQR, SolverDenseLU, SolverDenseCholesky, SolverDenseLDL
-from .common.solvers_sparse import SolverSparsePardiso, SolverSparseLU, SolverSparseCholeskyScikit, \
-    SolverSparseCholeskyCVXOPT
+from .common.solvers_sparse import SolverSparsePardiso, SolverSparseLU, SolverSparseCholeskyScikit, SolverSparseCholeskyCVXOPT
+
+# Modular inports
 from .core_objects import Signal, Module, Network, make_signals
+
+# Import modules
 from .modules.assembly import AssembleGeneral, AssembleStiffness, AssembleMass
 from .modules.autodiff import AutoMod
 from .modules.complex import MakeComplex, RealPart, ImagPart, ComplexNorm
 from .modules.filter import FilterConv, Filter, DensityFilter, OverhangFilter
-# Import from modules
 from .modules.generic import MathGeneral, EinSum, ConcatSignal
 from .modules.io import PlotDomain, PlotGraph, PlotIter, WriteToVTI
 from .modules.linalg import Inverse, LinSolve, EigenSolve, SystemOfEquations, StaticCondensation
 from .modules.scaling import Scaling
+
+# Further helper routines
 from .routines import finite_difference, minimize_oc, minimize_mma
 
 __all__ = [
@@ -34,7 +38,7 @@ __all__ = [
     'SolverSparsePardiso', 'SolverSparseLU', 'SolverSparseCholeskyScikit', 'SolverSparseCholeskyCVXOPT',
     # Modules
     "MathGeneral", "EinSum", "ConcatSignal",
-    "Inverse", "LinSolve", "EigenSolve", "SystemOfEquations", "StaticCondensation"
+    "Inverse", "LinSolve", "EigenSolve", "SystemOfEquations", "StaticCondensation",
     "AssembleGeneral", "AssembleStiffness", "AssembleMass",
     "FilterConv", "Filter", "DensityFilter", "OverhangFilter",
     "PlotDomain", "PlotGraph", "PlotIter", "WriteToVTI",
