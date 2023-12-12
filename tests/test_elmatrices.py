cdb538f330a465df75d9b0e6fef6af6cac543963
Joran van der Zwet <J.M.vanderZwet-1@tudelft.nl>
2023-12-12 14:50:09 +0100

    Implemented 3D versions of conductivity and mass element matrices test

--- tests/test_elmatrices.py
+++ tests/test_elmatrices.py
@@ -56,6 +56,48 @@ class TestElMats(unittest.TestCase):
 
 
     def TestMassMat3D(self):
+        N = 20
+        Lx, Ly, Lz = 2, 2, 2
+        lx, ly, lz = Lx/N, Ly/N, Lz/N
+        domain = pym.DomainDefinition(N,N,N, unitx = lx, unity = ly, unitz = lz)
+        rho = 1.0
+        mel = rho * np.prod(domain.element_size)
+
+        MEhc = np.zeros((domain.elemnodes * domain.dim, domain.elemnodes * domain.dim))
+        weights = np.array([8.0, 4.0, 2.0, 1.0])
+        for n1 in range(domain.elemnodes):
+            for n2 in range(domain.elemnodes):
+                dist = round(np.sum(abs(np.array(domain.node_numbering[n1]) - np.array(domain.node_numbering[n2]))) / 2)
+                MEhc[n1 * domain.dim + np.arange(domain.dim), n2 * domain.dim + np.arange(domain.dim)] = weights[dist]
+        MEhc *= mel / 216
+
+        ME = pym.ConsistentMassEq(domain, 3, 1.0)
+
+        self.assertEqual(ME, MEhc)
 
 
     def TestConductivityMat3D(self):
+        N = 20
+        Lx, Ly, Lz = 2, 2, 2
+        lx, ly, lz = Lx / N, Ly / N, Lz / N
+        domain = pym.DomainDefinition(N, N, N, unitx=lx, unity=ly, unitz=lz)
+        nodidx_left = domain.get_nodenumber(*np.meshgrid(0, range(domain.nely + 1), range(domain.nelz + 1))).flatten()
+        nodidx_right = domain.get_nodenumber(*np.meshgrid(domain.nelx, range(domain.nely + 1), range(domain.nelz + 1))).flatten()
+
+        kt = 1.0
+
+        s_x = pym.Signal('x', state=np.ones(domain.nel))
+        m_KT = pym.AssembleScalarField(s_x, domain=domain, bc=nodidx_left, kt=kt)
+        s_KT = m_KT.sig_out[0]
+        m_KT.response()
+
+        q = np.zeros(domain.nnodes)
+        Q = 1.0
+        q[nodidx_right] = Q / nodidx_right.size
+
+        # check with simple 1D heat conduction through wall
+        T_chk = Q * Lx / (kt * Ly * Lz)
+        T = np.linalg.solve(s_KT.state.toarray(), q)
+
+        npt.assert_allclose(T[nodidx_right[0]], 0, atol=1e-10)
+        npt.assert_allclose(T[nodidx_right[1]], T_chk, rtol=1e-10)
