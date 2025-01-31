9463e647629806310baebb154e8dc21ad77f1799
Joran van der Zwet <J.M.vanderZwet-1@tudelft.nl>
2025-01-31 13:37:23 +0100

    added _sensitivity to Transient Thermal

--- pymoto/modules/transient.py
+++ pymoto/modules/transient.py
@@ -30,3 +30,30 @@ class TransientThermal(Module):
             self.temperature[:, i + 1] = self.module_LinSolve.sig_out[0].state
 
         return self.temperature
+
+    def _sensitivity(self, dfdt):
+        # initialize adjoint variables
+        lams = np.zeros_like(self.temperature)
+        self.module_LinSolve.sig_in[1].state = -dfdt[:, -1]
+        self.module_LinSolve.response()
+        lams[:, -1] = self.module_LinSolve.sig_out[0].state
+
+        # perform adjoint solve for every time step
+        for i in reversed(range(self.steps)):
+            rhs = -dfdt[:, i] - self.mat_backward.dot(lams[:, i+1])
+            self.module_LinSolve.sig_in[1].state = rhs
+            self.module_LinSolve.response()
+            lams[:, i] = self.module_LinSolve.sig_out[0].state
+
+        # sensitivities to system matrices
+        tempsK = self.theta*self.temperature
+        tempsK[:, 1:] += (1-self.theta)*self.temperature[:, :-1]
+        dK = DyadCarrier(list(lams.T), list(tempsK.T))
+        dC = DyadCarrier(list(lams[:, 1:].T), list((1/self.dt)*np.diff(self.temperature).T))
+
+        # sensitivities to input heat
+        dQ = np.zeros_like(self.sig_in[2])
+        dQ[:, 1:] -= self.theta*lams[:, 1:]
+        dQ[:, :-1] += -(1-self.theta)*lams[:, 1:]
+
+        return dK, dC, dQ
