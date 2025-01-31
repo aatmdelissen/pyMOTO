809e5749633c1cf83f8f3e4c48f976df3c08dff6
Joran van der Zwet <J.M.vanderZwet-1@tudelft.nl>
2025-01-31 13:36:44 +0100

    added _response to Transient Thermal

--- pymoto/modules/transient.py
+++ pymoto/modules/transient.py
@@ -8,3 +8,25 @@ class TransientThermal(Module):
         self.dt = dt
         self.theta = theta
         self.module_LinSolve = LinSolve([Signal(), Signal()], **kwargs)
+
+    def _response(self, K, C, Q):
+        # prepare matrices for solve
+        Cstep = C.multiply(1 / self.dt)
+        K_forward = K.multiply(self.theta)
+        K_backward = K.multiply(1 - self.theta)
+        self.mat_forward = K_forward + Cstep
+        self.mat_backward = K_backward - Cstep
+        self.module_LinSolve.sig_in[0].state = self.mat_forward
+
+        # initialize temperatures
+        self.temperature = np.zeros((self.mat_forward.shape[0], self.steps + 1))
+        self.temperature[:, 0] = self.T_0
+
+        # perform solve for every time step
+        for i in range(self.steps):
+            rhs = self.theta * Q[:, i + 1] + (1 - self.theta) * Q[:, i] - self.mat_backward.dot(self.temperature[:, i])
+            self.module_LinSolve.sig_in[1].state = rhs
+            self.module_LinSolve.response()
+            self.temperature[:, i + 1] = self.module_LinSolve.sig_out[0].state
+
+        return self.temperature
